ba65c6488090ec24fecefd3cde8e8483c82c7684
wkra <wkrawiec@gmail.com>
2017-06-09 13:59:22 +0200

    Add files via upload

---
+++ script/lotto.js
@@ -0,0 +1,116 @@
+
+//moduł lotto
+var lottoModule = (function () {
+    var flagStop = false;
+    
+    //Twoje numery
+    var yourNum = function (){
+        var yourNumbers = [];
+        	while (yourNumbers.length<6){
+    	    var i = yourNumbers.length;
+    	    var flag = true;
+    	    
+    	    yourNumber = (prompt("Podaj liczbę od 1 do 49"));
+    	    
+    	    if (yourNumber === null){
+    	        flag = false;
+    	        flagStop=true;
+    	        yourNumbers=[0,0,0,0,0,0];
+
+    	    }
+            if (flag && !(yourNumber >=1 && yourNumber <=49)){
+                alert("Twoja liczba jest nieprawidłowa");
+    	        flag = false;
+            }
+    	    
+    	    for (k=0; k < yourNumbers.length; k++){
+    	            if (yourNumber === yourNumbers[k]){
+    	                alert("Podałeś już tą liczbę. Podaj inną od 1 do 49");
+    	                flag = false;
+    	            }
+    	    }
+    		
+    		if (flag && yourNumber != Math.floor(yourNumber) ){
+    			alert("Twoja liczba jest nieprawidłowa");
+    			flag = false;
+    		}
+    
+    	    if (yourNumber >=1 && yourNumber <=49 && flag) {
+    	        yourNumbers.push(yourNumber);
+    	    }
+    	    flag = true;
+    	}
+    	
+    	for (j = 0; j < yourNumbers.length; j++){
+    		console.log("Wybrana liczba " + (j+1) + ": " + yourNumbers[j]);
+    	}
+    	return yourNumbers;
+    };
+    
+    //Losowanie numerów
+    var draw = function (){
+    var drawNumbers = [];
+        var drawFlag = true;
+        for (i=0;drawNumbers.length<6;i++){
+            drawNumber = Math.round(Math.random()*49);
+            
+            for (m=0; m < drawNumbers.length; m++) {
+                if (drawNumber === drawNumbers[m]){
+                    drawFlag = false;
+                } 
+                
+            }
+            
+            if (drawNumber >= 1 && drawNumber <= 49 && drawFlag){
+                    drawNumbers.push(drawNumber);
+                    
+            } else {
+                console.log("Liczba nie przeszła walidacji: " + drawNumber);
+            }
+            drawFlag=true;
+        }
+        
+        for (j=0; j < drawNumbers.length; j++){
+            console.log("Wylosowana liczba " + (j+1) + " to: " + drawNumbers[j]);
+        }
+        return drawNumbers;
+    };
+    
+    //Porównanie numerów
+    var checkNumbers = function (a,b){
+        var numHits = 0;
+        var numbers = [];
+        
+        for (i=0; i< a.length; i++){
+            for (j=0; j<a.length; j++){
+                if (a[i] == b[j]){
+                    numHits++;
+                    numbers.push(a[i]);
+                }
+            }
+        }
+        console.log("Liczba trafień: " + numHits);
+        
+        for (k=0; k<numbers.length;k++){
+            console.log ("Trafione liczby to: " + numbers[k]);
+        }
+        if (!flagStop){
+            alert(
+            "Twoje liczby to: " + b+
+            "\n" + "Wylosowane liczby to: " + a +
+            "\n" + "Liczba trafień: " + numHits + 
+            "\n" + "Trafione liczby to: " + numbers
+        );
+        }
+    };
+    
+    // start game
+    var startGame = function  (){
+    	checkNumbers(draw(),yourNum());
+    };
+    
+    // przycisk
+    var start = document.querySelector("#startButton");
+    start.addEventListener('click',startGame);
+
+})();
---
+++ script/myjs.js
@@ -0,0 +1,99 @@
+$(document).ready(function() {
+	
+	// html
+	$("body").prepend('<div class="container"></div>');
+	$(".container").append('<h1>jQuery2</h1>');
+	$(".container").append('<section class="first"></section>');
+	$(".container").append('<section class="second"></section>');
+	$(".container").append('<section class="third"></section>');
+	
+		//first
+	$(".first").append("<nav></nav>")
+	$(".first nav").append($("<h3></h3>").text("Formularz"));
+	$(".first").append('<article></article>')
+	$(".first article").append('<form class="myform"></form>');
+	$(".myform").append('<input class="form-control myInput type="text" placeholder="wpisz text">')
+	$(".first article").append("<br>");
+	$(".first article").append('<div class="myInput1"><p></p></div>')
+	$(".first article").append('<div class="myInput2"><p></p></div>')
+	
+	
+		//second
+	$(".second").append("<nav></nav>")
+	$(".second nav").append('<h3>jQuery ajax</h3>');
+	$(".second").append("<article></article>");
+	$(".second article").append('<nav></nav>');
+	$(".second article nav").append('<div class="btn-group"></div>')
+	$(".second article").append('<br>');
+	$(".second article").append('<div class="innerHTML""></div>');
+	$(".second article").append('<div class="adjacentHTML"></div>')
+	
+		//third
+	$(".third").append("<nav></nav>")
+	$(".third nav").append('<h3>Lotto</h3>');
+	$(".third").append("<article></article>");
+	
+		// btns
+	for (i=0;i<4;i++){
+		var files = ["file1.txt", "file2.php", "file3.html", "file4.xml"];
+		var btn1 = '<button class="btn btn-primary myBtn" href="';
+		var btn2 = '">Button</button>';
+
+		$(".btn-group").append(btn1 + (files[i]) + btn2);
+	}
+	
+	// /html
+	
+	// btns event
+	$(".myBtn").click(function(){
+		$(".innerHTML").load($(this).attr("href")).fadeIn("slow");
+	});
+	
+	// load first ajax page
+	$(".innerHTML").load("plik1.txt");
+
+
+	// input value
+	setInterval(function(){
+		var myVal = $('.myInput').val();
+		$(".myInput1 p").text(myVal);
+		$(".myInput2 p").text(reverse(myVal));
+		
+	}, 500);
+	
+	function reverse (a){
+		var b = "";
+		if (a.length>0){
+		for (i=a.length-1;i>=0;i--){
+			b += a[i];
+		}
+	}
+		return b;
+	}
+	
+	// hide section
+	$("article").hide();
+	$("nav").click(function(){
+		$(this).next("article").slideToggle();
+		return false;
+	});
+
+	// hamburger
+	$("section>nav").prepend('<div class="myHamburger"></div>');
+	
+	// third artice
+	$(".third article").append('<h1>Gra Lotto</h1><br /><input class="btn btn-primary" type="button" value="S T A R T" id="startButton"/>');
+	
+	// lotto.js
+	(function() {
+
+    var lotto = document.createElement('script');
+        lotto.type = 'text/javascript';
+        lotto.async = true;
+        lotto.src = 'lotto.js';
+
+    var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(lotto, s);
+
+})();
+});
